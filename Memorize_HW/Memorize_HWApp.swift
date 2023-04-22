a710ff2603ea718aca56821f07cd80187ff397ea
Tiffany <yang.ding@hotmail.com>
2023-04-22 11:40:26 -0700

    Initial Commit

---
+++ Memorize_HW/Memorize_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_HWApp.swift
+//  Memorize_HW
+//
+//  Created by Yang Ding on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
