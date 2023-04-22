0068a8bcd02e9c5f69993afe0744322a15943f66
Tiffany <yang.ding@hotmail.com>
2023-04-22 12:34:28 -0700

    set up basic layout screen

--- Memorize_HW/ContentView.swift
+++ Memorize_HW/ContentView.swift
@@ -8,11 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	var emojis = ["🐶", "🐹", "🦁", "🐸", "🐻‍❄️", "🦄", "🐷", "🐻"]
+	
+	@State var emojiCount = 8
+	
+	var body: some View {
+		VStack {
+			// title
+			Text("Memorize!")
+			// cards
+			ScrollView {
+				LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
+					ForEach(emojis[0..<emojiCount], id: \.self, content: { emoji in
+						CardView(isFaceUp: true, content: emoji).aspectRatio(2/3, contentMode: .fit)
+					})
+				}
+			}
+			// theme chooser
+			HStack {
+				VStack {
+					Button {
+						// change current view to this category
+					} label: {
+						Image(systemName: "pawprint")
+					}
+					Text("Animals")
+				}
+				VStack {
+					Button {
+						// change current view to this category
+					} label: {
+						Image(systemName: "sportscourt")
+					}
+					Text("Sports")
+				}
+				VStack {
+					Button {
+						// change current view to this category
+					} label: {
+						Image(systemName: "fork.knife")
+					}
+					Text("Foods")
+				}
+			}
+		}
+	}
 }
+
+struct CardView: View {
+	var isFaceUp: Bool
+	var content: String
+	var body: some View {
+		ZStack {
+			RoundedRectangle(cornerRadius: 5)
+			Text(content)
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
