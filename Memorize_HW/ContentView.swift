7fee24d302d7689ab25f6a8e3635d48c61c0762d
Tiffany <yang.ding@hotmail.com>
2023-04-23 10:23:05 -0700

    add logic to theme buttons

--- Memorize_HW/ContentView.swift
+++ Memorize_HW/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
 	@State var emojiCategories = [
 		"animals": ["🐶", "🐹", "🦁", "🐸", "🐻‍❄️", "🦄", "🐷", "🐻", "🐥", "🐙", "🦦"],
-		"sports": ["sport"],
+		"sports": ["⚽️", "🏓", "🏈", "🏹", "🏀", "🧗🏻‍♀️", "🤺", "🏋🏻‍♀️", "🎾", "🥊", "🏂", "⛳️", "🏐"],
+		"weather": ["☀️", "🌦", "❄️", "⛈", "☁️", "🌪", "☄️", "🌈"]
 	]
+	@State var emojis = ["🐶", "🐹", "🦁", "🐸", "🐻‍❄️", "🦄", "🐷", "🐻", "🐥", "🐙", "🦦"]
 	
-	@State var emojiCount = 1
+	
 	
 	var body: some View {
 		VStack {
@@ -22,46 +24,59 @@ struct ContentView: View {
 			// cards
 			ScrollView {
 				LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
-					ForEach(emojiCategories["sports"]![0..<emojiCount], id: \.self, content: { emoji in
+					ForEach(emojis[0..<emojis.count], id: \.self, content: { emoji in
 						CardView(isFaceUp: true, content: emoji).aspectRatio(2/3, contentMode: .fit)
 					})
 				}
 			}
 			.font(.largeTitle)
 			.foregroundColor(.init(hue: 0.08, saturation: 0.52, brightness: 0.90))
+//			CardGrid(emojis: emojiCategories["animals"]!, emojiCount: emojiCategories["animals"]!.count)
 			Spacer()
 			// theme chooser
 			HStack(alignment: .bottom) {
 				VStack {
 					Button {
 						// change current view to this category
+						emojis = emojiCategories["animals"]!.shuffled()
 					} label: {
 						Image(systemName: "pawprint")
 							.padding(.bottom, -5.0)
 					}.font(.title2)
 					Text("Animals").font(.subheadline)
 				}
+				.onTapGesture {
+					emojis = emojiCategories["animals"]!.shuffled()
+				}
 				Spacer()
 				VStack {
 					Button {
 						// change current view to this category
+						emojis = emojiCategories["sports"]!.shuffled()
 					} label: {
 						Image(systemName: "sportscourt")
 							.padding(.bottom, -5.0)
 					}.font(.title2)
 					Text("Sports").font(.subheadline)
 				}
+				.onTapGesture {
+					emojis = emojiCategories["sports"]!.shuffled()
+				}
 				Spacer()
 				VStack {
 					Button {
 						// change current view to this category
+						emojis = emojiCategories["weather"]!.shuffled()
 					} label: {
 						Image(systemName: "cloud")
 							.padding(.bottom, -4.0)
 					}.font(.title2)
 					Text("Weather").font(.subheadline)
 				}
-			}.padding(/*@START_MENU_TOKEN@*/[.top, .leading, .trailing], 20.0/*@END_MENU_TOKEN@*/).foregroundColor(.blue)
+				.onTapGesture {
+					emojis = emojiCategories["weather"]!.shuffled()
+				}
+			}.padding(/*@START_MENU_TOKEN@*/[.top, .leading, .trailing], 20.0/*@END_MENU_TOKEN@*/).foregroundColor(Color(hue: 1.0, saturation: 0.821, brightness: 0.505))
 		}
 		.padding(/*@START_MENU_TOKEN@*/[.top, .leading, .trailing]/*@END_MENU_TOKEN@*/)
 	}
@@ -77,6 +92,7 @@ struct CardView: View {
 		}
 	}
 }
+
 
 
 
